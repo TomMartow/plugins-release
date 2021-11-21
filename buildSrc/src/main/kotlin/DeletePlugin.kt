74443322b28ca8076232652afef1803f4fe02ec7
NoMore <thomasbiggin94+github@gmail.com>
2021-11-21 21:32:22 +0000

    Update

--- buildSrc/src/main/kotlin/DeletePlugin.kt
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.register
-
-class DeletePlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        project.tasks.register<DeleteTask>("delete") {}
-    }
-}
--- buildSrc/src/main/kotlin/ObfuscatePlugin.kt
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.register
-
-class ObfuscatePlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        project.tasks.register<ObfuscateTask>("obfuscatePlugins") {}
-    }
-}
